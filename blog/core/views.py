29b595522298a08422f3c4371b5111f3a7427633
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-04-13 22:24:59 -0300

    Add index view

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.core.utils.parsers.markdown import MarkdownParser
+
+def index(request):
+    markdown_parser = MarkdownParser()
+
+    context = {'posts': markdown_parser.get_html_strings_list_with_metadata()}
+
+    return render(request, 'core/list.html', context)
